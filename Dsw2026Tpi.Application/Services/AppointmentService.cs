433cc8b5beebd10f8842c822d90071e1683b32d8
Martina Gomez <martina.gomez139@gmail.com>
2026-08-01 12:07:41 -0300

    Logica final de turnos y correccion entidad Appointment

--- Dsw2026Tpi.Application/Services/AppointmentService.cs
+++ Dsw2026Tpi.Application/Services/AppointmentService.cs
@@ -1,6 +1,8 @@
 ﻿using Dsw2026Tpi.Application.Dtos;
 using Dsw2026Tpi.Application.Interfaces;
 using Dsw2026Tpi.Data;
+using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2026Tpi.Application.Services;
 
@@ -13,6 +15,20 @@ public class AppointmentService : IAppointmentService
     }
     public async Task <AppointmentModel.Response> CreateAppointmentAsync(AppointmentModel.Request request)
     {
+        bool isSlotTaken= await _context.Appointments 
+           .AnyAsync(a => a.AvailabilitySlotId == request.AvailabilitySlotId );
+        if (isSlotTaken)
+        {
+            throw new Exception("Horario seleccionado no disponible.");
+        }
+        var newAppointment = new Appointment(
+           request.AvailabilitySlotId,
+           request.PatientId,
+           request.Reason
+       );
+        _context.Appointments.Add(newAppointment);
+        await _context.SaveChangesAsync();
+        
         return new AppointmentModel.Response(
             Guid.NewGuid(),
             "Reservado",
--- Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
+++ Dsw2026Tpi.Data/Dsw2026TpiDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Dsw2026Tpi.Data;
@@ -9,7 +10,7 @@ public class Dsw2026TpiDbContext: DbContext
         base(options)
     {
     }
-
+    public DbSet<Appointment> Appointments { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
