d552f6e3090b335e08a431b6270d6398ff48340f
Nicolas Perry <nicolasperry04@gmail.com>
2026-07-31 21:39:34 -0300

    Agrego migracion de pacientes

---
+++ Dsw2026Tpi.Data/Migrations/20260801002256_Add_Patient_Email_And_UserId.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dsw2026Tpi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Patient_Email_And_UserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Patients",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Patients",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_Email",
+                table: "Patients",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_UserId",
+                table: "Patients",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_Email",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_UserId",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Patients");
+        }
+    }
+}
