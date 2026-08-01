d0d50c3853963b7616a97f143ba7fca23b7cef20
Nazarena Martinez <nazarenamartinez91@gmail.com>
2026-08-01 18:57:31 -0300

    CRUD de medicos

--- Dsw2026Tpi.Application/Dtos/DoctorModel.cs
+++ Dsw2026Tpi.Application/Dtos/DoctorModel.cs
@@ -5,4 +5,7 @@ public record DoctorModel
     public record Request(string Name, string LicenseNumber, Guid SpecialityId);
     public record Response(Guid Id, string Name, string LicenseNumber, SpecialityDto? Speciality);
     public record SpecialityDto(Guid? SpecialityId, string? Name);
+
+    // para GET /doctors/{id}/availabilities
+    public record AvailabilityResponse(string Day, string StartTime, string EndTime);
 }
