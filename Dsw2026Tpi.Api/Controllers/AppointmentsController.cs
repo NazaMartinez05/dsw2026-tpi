e643581399276ded8c1949b746bc212ca99c2c63
Nazarena Martinez <nazarenamartinez91@gmail.com>
2026-08-02 20:01:08 -0300

    Agrego endpoint CreateAppointment

--- Dsw2026Tpi.Api/Controllers/AppointmentsController.cs
+++ Dsw2026Tpi.Api/Controllers/AppointmentsController.cs
@@ -1,23 +1,28 @@
 ﻿using Dsw2026Tpi.Application.Dtos;
 using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2026Tpi.Api.Controllers;
 
 [ApiController]
 [Route("api/appointments")]
-public class AppointmentsController : ControllerBase
+public class AppointmentsController : AppController
 {
     private readonly IAppointmentService _appointmentService; 
     public AppointmentsController(IAppointmentService appointmentService)
     {
         _appointmentService = appointmentService;
     }
+
     [HttpPost]
-    public IActionResult CreateAppointment([FromBody] AppointmentModel.Request request)
+    public async Task<IActionResult> CreateAppointment([FromBody] AppointmentModel.Request request)
     {
-        return Ok(new { mesagge = "Endpoint funionando" });
+        var response = await _appointmentService.CreateAppointmentAsync(request);
+
+        return CreatedAtAction(nameof(CreateAppointment), new { }, response);
     }
+
     [HttpGet("patient")]
     public async Task<IActionResult> GetPatientAppointments([FromQuery] long dni)
     {
@@ -31,6 +36,7 @@ public class AppointmentsController : ControllerBase
             return BadRequest(new { message = ex.Message});
         }
     }
+
     [HttpDelete("{id}")]
     public async Task<IActionResult> CancelAppointment(Guid id)
     {
@@ -44,6 +50,7 @@ public class AppointmentsController : ControllerBase
             return BadRequest(new { message = ex.Message});
         }
     }
+
     [HttpGet]
     public async Task<IActionResult> GetAppointmentsByDate(
         [FromQuery] DateTime date,
@@ -61,12 +68,13 @@ public class AppointmentsController : ControllerBase
         }           
 
     }
+
     [HttpGet("search")]
     public async Task<IActionResult> SearchAppointmetns(
         [FromQuery] Guid? specialityId,
         [FromQuery] Guid? doctorId,
         [FromQuery] string? dni,
-        [FromQuery] DateTime? date,
+        [FromQuery] DateTime date,
         [FromQuery] int pageSize =10,
         [FromQuery] int pageIndex=1)
     {
