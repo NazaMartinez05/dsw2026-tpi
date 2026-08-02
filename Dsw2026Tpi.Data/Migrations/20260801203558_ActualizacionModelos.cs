3ba9b57fc6ffb7d2d550046e353d2350806ca113
Nazarena Martinez <nazarenamartinez91@gmail.com>
2026-08-02 20:00:22 -0300

    Elimino columnas duplicadas de email

--- Dsw2026Tpi.Data/Migrations/20260801203558_ActualizacionModelos.cs
+++ Dsw2026Tpi.Data/Migrations/20260801203558_ActualizacionModelos.cs
@@ -10,31 +10,7 @@ namespace Dsw2026Tpi.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "Email",
-                table: "Patients",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValue: "");
 
-            migrationBuilder.AddColumn<string>(
-                name: "UserId",
-                table: "Patients",
-                type: "nvarchar(450)",
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Patients_Email",
-                table: "Patients",
-                column: "Email",
-                unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Patients_UserId",
-                table: "Patients",
-                column: "UserId",
-                unique: true,
-                filter: "[UserId] IS NOT NULL");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Appointments_AvailabilitySlotId",
